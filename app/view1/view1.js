1e24c7a3317fbfa472a352521d848f0744dc3f19
Attila Buturla <AttilaButurla@gmail.com>
2014-12-06 11:47:59 -0800

    Added some real basic UI to get started

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,13 +10,32 @@ angular.module('myApp.view1', ['ngRoute', 'firebase'])
 }])
 
 .controller('View1Ctrl', ['$scope', '$firebase', function($scope, $firebase) {
+    var ref = new Firebase("https://vivid-fire-4038.firebaseio.com/users");
+    var fb = $firebase(ref);
 
-      var ref = new Firebase("https://vivid-fire-4038.firebaseio.com/users");
-      var fb = $firebase(ref);
+    // sync as object
+    var syncObject = fb.$asObject();
 
-      $scope.reset = function() {
-        fb.$set({user: 'Attila'});
-      };
+        $scope.foo = 'blah';
 
-      $scope.reset();
+    // three way data binding
+    syncObject.$bindTo($scope, 'users');
+
+   $scope.debug = function(){
+       console.log($scope);
+   };
+
+    $scope.reset = function() {
+        fb.$set(
+            {
+                username: 'Attila',
+                days: [
+                    {date: 0, breakfast: '', morningSnack: '', lunch : '', afternoonSnack:'', dinner: ''},
+                    {date: 1, breakfast: '', morningSnack: '', lunch : '', afternoonSnack:'', dinner: ''},
+                    {date: 2, breakfast: '', morningSnack: '', lunch : '', afternoonSnack:'', dinner: ''}
+                ]
+            });
+
+      console.log('reset');
+    };
 }]);
