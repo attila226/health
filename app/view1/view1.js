a6967a7123ddf1a85f765c6f7bef42c756a8ff1f
Attila Buturla <AttilaButurla@gmail.com>
2014-12-05 22:42:03 -0800

    Added firebase sample code

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'firebase'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
@@ -9,6 +9,14 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', '$firebase', function($scope, $firebase) {
 
+      var ref = new Firebase("https://vivid-fire-4038.firebaseio.com/users");
+      var fb = $firebase(ref);
+
+      $scope.reset = function() {
+        fb.$set({user: 'Attila'});
+      };
+
+      $scope.reset();
 }]);
