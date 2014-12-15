590b9a12740af19da9af4bcc903cec632a74b250
Attila Buturla <AttilaButurla@gmail.com>
2014-12-14 23:06:14 -0800

    Added grid to food log

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,9 +1,9 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute', 'firebase', 'ui.bootstrap',])
+angular.module('myApp.view1', ['ngRoute', 'firebase', 'ui.bootstrap'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
+  $routeProvider.when('/Log', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
   });
@@ -11,29 +11,30 @@ angular.module('myApp.view1', ['ngRoute', 'firebase', 'ui.bootstrap',])
 
 .controller('View1Ctrl', ['$scope', '$firebase', function($scope, $firebase) {
     //TODO: Modify to get a single user from an array of users
-    var ref = new Firebase("https://vivid-fire-4038.firebaseio.com/users");
+    var ref = new Firebase("https://vivid-fire-4038.firebaseio.com/users/0/days");
     var fb = $firebase(ref);
 
     // sync as object
     var syncObject = fb.$asObject();
 
     // three way data binding
-    syncObject.$bindTo($scope, 'users');
+    syncObject.$bindTo($scope, 'days');
 
    $scope.debug = function(){
        console.log($scope);
    };
 
+    //Need to point to users
     $scope.reset = function() {
         fb.$set(
-            {
+            [{
                 username: 'Attila',
                 days: [
-                    {date: 0, breakfast: '', morningSnack: '', lunch : '', afternoonSnack:'', dinner: ''},
-                    {date: 1, breakfast: '', morningSnack: '', lunch : '', afternoonSnack:'', dinner: ''},
-                    {date: 2, breakfast: '', morningSnack: '', lunch : '', afternoonSnack:'', dinner: ''}
+                    {date: '12/14/2014', breakfast: 'scrambleded eggs, smoothie', morningSnack: 'Bananna', lunch: 'chicken, vegatables', afternoonSnack: '', dinner: 'small salad'},
+                    {date: '12/15/2014', breakfast: '', morningSnack: '', lunch: '', afternoonSnack: '', dinner: ''},
+                    {date: '12/16/2014', breakfast: '', morningSnack: '', lunch: '', afternoonSnack: '', dinner: ''}
                 ]
-            });
+            }]);
 
       console.log('reset');
     };
